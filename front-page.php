cfcc003034c559781639c74d1e28bcaea4c9d9ff
manno-k <art.kuromi@gmail.com>
2020-07-23 23:47:46 +0800

    pc news section 作成

--- front-page.php
+++ front-page.php
@@ -19,14 +19,92 @@
 
 			<article class="l-front">
 				<section class="l-front__story">
-					<h2>OUR STORY</h2>
-					<h3>blue-eyed girl</h3>
+					<div class="l-front__story-title">
+						<h2>OUR STORY</h2>
+						<h3>blue-eyed
+							girl<a class="u-link-gold u-hidden-md-down" href="<?php echo home_url('/salon'); ?>">
+								MORE
+							</a>
+						</h3>
+
+					</div>
 					<div class="l-front__story-content">
 						<?php the_field('our_story') ?>
 					</div>
-					<a class="u-link-gold" href="<?php echo home_url('/salon'); ?>">
+					<a class="u-link-gold u-hidden-lg-up" href="<?php echo home_url('/salon'); ?>">
 						MORE
 					</a>
+				</section>
+				<section class="l-front__news">
+					<div class="l-front__news-left"></div>
+					<div class="l-front__news-right">
+						<h2>Latest News</h2>
+					</div>
+					<?php
+						$paged    = get_query_var('paged') ? get_query_var('paged') : 1;
+						$my_query = new WP_Query(array(
+							'paged'          => $paged,
+							'posts_per_page' => 4
+						));
+					?>
+					<div class="l-front__news-left bottom">
+						<div class="js-news">
+							<?php if ($my_query->have_posts()): while ($my_query->have_posts()): $my_query->the_post(); ?>
+								<a href="<?php the_permalink(); ?>">
+									<?php
+										if (wp_is_mobile()) {
+											$img_size = 'medium';
+										} else {
+											$img_size = 'large';
+										}
+										if (has_post_thumbnail()): ?>
+											<?php the_post_thumbnail($img_size); ?>
+										<?php else: ?>
+											<img src="<?php echo get_template_directory_uri(); ?>/assets/img/noimage.jpg" alt="no image">
+										<?php endif; ?>
+								</a>
+
+							<?php endwhile;endif;
+								wp_reset_postdata();
+							?>
+						</div>
+					</div>
+					<div class="l-front__news-right bottom">
+						<?php
+							$paged    = get_query_var('paged') ? get_query_var('paged') : 1;
+							$my_query = new WP_Query(array(
+								'paged'          => $paged,
+								'posts_per_page' => 4
+							));
+						?>
+						<?php if ($my_query->have_posts()): while ($my_query->have_posts()): $my_query->the_post(); ?>
+							<a class="post" href="<?php the_permalink(); ?>">
+								<span class="cat"><?php
+										$category = get_the_category();
+										echo $category[0]->cat_name;
+									?></span>
+								<time>
+									<?php the_time('Y-m-d'); ?>
+								</time>
+								<h3>
+									<?php
+										if (mb_strlen($post->post_title, 'UTF-8') > 13) {
+											$title = mb_substr($post->post_title, 0, 13, 'UTF-8');
+											echo $title . '…';
+										} else {
+											echo $post->post_title;
+										}
+									?>
+								</h3>
+							</a>
+						<?php endwhile;endif;
+							wp_reset_postdata();
+						?>
+						<a class="more" href="<?php echo home_url('/blog'); ?>">
+							READ MORE
+						</a>
+						<div class="hr"></div>
+					</div>
 				</section>
 			</article>
 		</main><!-- #main -->
--- src/js/slick-init.js
+++ src/js/slick-init.js
@@ -2,22 +2,13 @@
 詳しい解説
 https://webdesignday.jp/inspiration/technique/jquery-js/3847/
  */
-jQuery(function ($){
-  $('.js-case').slick({
-    slidesToShow:3,
-    responsive:[
-      {
-        breakpoint: 768,
-        settings:{
-          slidesToShow:3,
-        }
-      },
-      {
-        breakpoint: 480,
-        settings:{
-          slidesToShow:1,
-        }
-      },
-    ]
+jQuery(function ($) {
+  $('.js-news').slick({
+    slidesToShow: 1,
+    autoplay:true,
+    autoplaySpeed:5000,
+    dots:false,
+    arrows:true,
+
   });
 });
